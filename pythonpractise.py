575403236e6ab31040e384f34ff8602fb5003364
duktar210143 <duktar210143@gmail.com>
2021-06-14 21:19:11 +0545

    pyhtonpractisefileadded

---
+++ pythonpractise.py
@@ -0,0 +1,135 @@
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+# c=int(input("enter a number"))
+# if a==b:
+#     print("1")
+# elif a>b:
+#     print("2")
+# else:
+#     print("3")
+#
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+# c=int(input("enter a number"))
+# d=int(input("enter a number"))
+# if a==b and c==d:
+#     print("hello world")
+# else:
+#     print("wrong numbers")
+
+#or
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+# c=int(input("enter a number"))
+# d=int(input("enter a number"))
+#
+# if a==b or c==d:
+#     print("hello world")
+# else:
+#     print("wrong numbers")
+
+
+#4
+# x=int(input("enter a number\t"))
+# if x>0:
+#     print("true")
+# elif x<0:
+#     print("false")
+# elif x==0:
+#     print("zero")
+
+
+#5
+# num=int(input("enter a number"))
+# if num%2==0:
+#     print(f"the number {num} is even")
+# else:
+#     print(f"the number {num} is odd")
+
+
+#6
+# math=int(input("enter your marks for math"))
+# science=int(input("enter your marks science "))
+# social=int(input("enter your marks for social"))
+# total_marks=(math+social+science)
+# percentage=(total_marks*100)/300
+#
+# if percentage>=70:
+#     print(f"your marks is {total_marks} your total percentage is {percentage} you garde is a")
+# elif percentage>=40:
+#     print(f"you passeed with {total_marks} and percentage {percentage}")
+# else:
+#     print("you failed")
+#
+
+
+# name=input("enter your name\n")
+# age=int(input("enter your age\n"))
+# address=input("enter your address")
+#
+# print(name,("\n"), age,("\n")  , address,("\n"))
+
+#area of a circle
+
+# r=int(input("enter a radius:"))
+# pi=3.14
+# area=pi*r**2
+# print(f"area of circle is {area}")
+
+# weight=float(input("enter your weight in kg:"))
+# height=float(input("enter your height in meter:"))
+# BMI=weight/height
+# print(f"your BMI is {BMI}")
+#21
+# a=int(input("enter the number of st in a:"))
+# b=int(input("enter the number of st in b:"))
+# c=int(input("enter the number of st in c"))
+# d1=a//2
+# print("the required number of desk for first class",d1)
+# d2=b//2
+# print("the required nu,ber of desk for second class",d2)
+# d3=c//2
+# print("the required number of desk for third class",d3)
+# #remaining desks
+# r1=a%2
+# print("the remaining number of desk for first class",r1)
+# r2=b%2
+# print("the remaining number of desk for second classs ",r2)
+# r3=c%2
+# print("the remaining number of desk fpr third class",r3)
+# total_desk=d1+d2+d3+r1+r2+r3
+# print(f"tnhe minium number of desk required is {total_desk}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
